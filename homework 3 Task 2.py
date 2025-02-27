9051ccc5ae9b5443fa66513611421f1ecb05fdcd
Oleksandr Smyrnov <recovery20101@gmail.com>
2025-02-27 14:19:37 +0200

    import

--- homework 3 Task 2.py
+++ homework 3 Task 2.py
@@ -1,24 +1,22 @@
 import random
 
-def get_numbers_ticket(min, max, quantity):                           # оголушуємо функцію
+def get_numbers_ticket(min, max, quantity):                                   # оголушуємо функцію
     try:
-        min = int (min)                                               # оголушуємо змінні
-        max = int (max)
-        quantity = int (quantity)
+        min = int(min)                                                        # оголушуємо змінні
+        max = int(max)
+        quantity = int(quantity)
         list_of_numbers = []
-        if min < 1:                                                   # перевірка параметрів на валідність
-            return list_of_numbers
-        if max > 1000:
-            return list_of_numbers
-        elif  min <= max and quantity <= max:                         # виконання основного блоку                     
-            list_of_numbers = range(min, max + 1)
-            output_numbers = random.sample(list_of_numbers, quantity)
-            output_numbers.sort()
-            return output_numbers
-        else:
-            return list_of_numbers
-    except ValueError:                                                # виконнаня блоку, якщо введене не число
-        return print ('One of the value is not a number')
+        if min < 1 or max > 1000 or min > max or quantity > (max - min + 1):  # перевірка параметрів на валідність
+            return []
+                           
+        list_of_numbers = range(min, max + 1)
+        output_numbers = random.sample(list_of_numbers, quantity)
+        output_numbers.sort()
+        return output_numbers
+        
+    except ValueError:                                                         # виконнаня блоку, якщо введене не число
+        print ('One of the value is not a number')
+        return []
 
 min = input("Введіть мінімальне число (не менше 1): ")                # ввод данних користувачем
 max = input("Введіть максимальне число (не більше 1000): ")
